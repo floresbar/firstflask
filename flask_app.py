8f7f107d8313fbcb35d2051df97ccf53c68b59ac
floresbar <e.floresbartelt@outlook.com>
2021-12-04 18:22:13 +0100

    Fake wine data

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -7,6 +7,12 @@ app.config["DEBUG"] = True
 def hello_world():
     return "<p>Hello, from TechLabs!</p>"
 
-@app.route("/hello/")
-def hello():
-    return f"<p>hello!</p>"
+wines = [
+    {"id": 0, "alcohol": 8, "quality": 10},
+    {"id": 1, "alcohol": 12, "quality": 8},
+    {"id": 2, "alcohol": 10.5, "quality": 9},
+]
+
+@app.route("/api/wines/all", methods=["GET"])
+def return_all():
+    return jsonify(wines)
