cd0de6631b0f8fde78c9a9a6aaf839ff27b54a26
floresbar <e.floresbartelt@outlook.com>
2021-12-04 18:46:04 +0100

    Added read from csv

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,10 @@
-from flask import Flask, jsonify, request
+import os
+import json
+from flask import Flask, jsonify, request, Response
+import pandas as pd
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, from TechLabs!</p>"
 
 wines = [
     {"id": 0, "alcohol": 8, "quality": 10},
@@ -13,9 +12,20 @@ wines = [
     {"id": 2, "alcohol": 10.5, "quality": 9},
 ]
 
+module_dir = os.path.abspath(os.path.dirname(__file__))
+file_path = os.path.join(module_dir, "winequality-white.csv")
+df = pd.read_csv(file_path, sep=";")
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, from TechLabs!</p>"
+
+
 @app.route("/api/wines/all", methods=["GET"])
 def return_all():
-    return jsonify(wines)
+    return Response(df.to_json(orient="index"), mimetype="application/json")
+
 
 @app.route("/api/wines", methods=["GET"])
 def get_wine_by_id():
@@ -24,10 +34,5 @@ def get_wine_by_id():
     else:
         return "Error: No id field provided. Please specify an id."
 
-    results = []
-
-    for wine in wines:
-        if wine["id"] == id:
-            results.append(wine)
-
-    return jsonify(results)
+    row = df.iloc[[id]]
+    return Response(row.to_json(orient="index"), mimetype="application/json")
